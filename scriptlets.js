bd4ff2f33a09ee3306c20f21cb538fed7f2ba390
givillow <36812364+givillow@users.noreply.github.com>
2021-07-10 16:30:54 +0000

    Update scriptlets.js

--- scriptlets.js
+++ scriptlets.js
@@ -99,27 +99,36 @@
 /// run-script.js
 /// alias rs.js
 // example.com##+js(rns, dist/script.js)
-(() => { 
-	'use strict';
-	const script = '{{1}}';
-	//if ( script === '' || script === '{{1}}' ) { return; }
-	if ( script === '' ) { return; }
-	const log = console.log.bind(console);
-	log('uBO: run-script("%s")', script);
-	
-	const addScript = () => {
-		const node = document.createElement('script');
-		node.src = script;
-		document.body.appendChild(node);
-		log('uBO: run-script(): executed');
-	}
-	if ( document.readyState !== 'complete' ) {
-		window.addEventListener('load', addScript, { once: true });
-	} else if ( document.readyState === 'loading' ) {
-		window.addEventListener('DOMContentLoaded', addScript, { once: true });
-	} else {
-		addScript();
-	}
+(() => {
+  'use strict';
+  const script = '{{1}}';
+  if ( script === '' || script === '{{1}}' ) { return; }
+  // if ( script === '' ) { return; }
+  const log = console.log.bind(console);
+  log('uBO: run-script("%s")', script);
+
+  // https://gist.github.com/jlong/2428561
+  const parser = document.createElement('a');
+  parser.href = script;
+  if ( window.location.hostname !== parser.hostname) {
+    log('uBO: run-script(): hostname mismatch');
+    return;
+  }
+
+  const addScript = () => {
+    const node = document.createElement('script');
+    node.src = parser.href;
+    document.body.appendChild(node);
+    log('uBO: run-script(): executed');
+  };
+
+  if ( document.readyState !== 'complete' ) {
+    window.addEventListener('load', addScript, { once: true });
+  } else if ( document.readyState === 'loading' ) {
+    window.addEventListener('DOMContentLoaded', addScript, { once: true });
+  } else {
+    addScript();
+  }
 })();
 
 /// set-attr.js
